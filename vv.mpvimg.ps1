99b27d94a9d2ff4af37b3e31edaaa024ff4726f7
HRXN <Hrxn@users.noreply.github.com>
2021-12-06 16:37:19 +0100

    Also change the name in the file part deux

--- vv.mpvimg.ps1
+++ vv.mpvimg.ps1
@@ -1,5 +1,5 @@
 if (($args[0] -eq $null) -or ($args[1] -eq $null)) {
-	Write-Host "[fs.mpvimg] Usage: fs.mpvimg.ps1 <filename> <start position in seconds> [<duration to process in seconds>]"
+	Write-Host "[vv.mpvimg] Usage: vv.mpvimg.ps1 <filename> <start position in seconds> [<duration to process in seconds>]"
 } else {
 	$File = Get-Item $args[0]
 	$Name = $file.BaseName
@@ -10,9 +10,9 @@ if (($args[0] -eq $null) -or ($args[1] -eq $null)) {
 		$tcen = $args[1] + 2
 	}
 	if (Test-Path -LiteralPath $Name -PathType Container) {
-		Write-Host "[fs.mpvimg] Info: Deleting previously existing directory `"$Name`" ... "
+		Write-Host "[vv.mpvimg] Info: Deleting previously existing directory `"$Name`" ... "
 		Remove-Item -LiteralPath $Name -Recurse -Force
 	}
 	New-Item -Path "." -Name $Name -ItemType Directory
-	& mpv.com --no-audio --fs --vo=image --vo-image-format=png --vo-image-png-compression=0 --vo-image-tag-colorspace=yes --vo-image-high-bit-depth=yes --vo-image-outdir=$name --start=$tcst --end=$tcen $File
+	& mpv --no-audio --fs --vo=image --vo-image-format=png --vo-image-png-compression=0 --vo-image-tag-colorspace=yes --vo-image-high-bit-depth=yes --vo-image-outdir=$name --start=$tcst --end=$tcen $File
 }
