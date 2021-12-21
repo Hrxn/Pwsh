3f93b370b5c9899519c639aedf448589aa6cec6e
HRXN <Hrxn@users.noreply.github.com>
2021-12-21 14:12:34 +0100

    Final fix for ma.clean.ps1
    Saves and restores the PSDrive state, so, everything is working now..

--- ma.clean.ps1
+++ ma.clean.ps1
@@ -1,8 +1,8 @@
 # -------------------------------------------------- Declarations for this entire Scope -------------------------------------------------- #
 $StatusVal = [Byte] 0
 $PathParam = [String]::Empty
-$PrimColor = [System.ConsoleColor]::White
-$ScndColor = [System.ConsoleColor]::DarkGray
+$PrimColor = [ConsoleColor]::White
+$ScndColor = [ConsoleColor]::DarkGray
 
 # --------------------------------------------------------- Function Definitions --------------------------------------------------------- #
 function Print-Unava {
@@ -25,6 +25,25 @@ function Print-Notmp {
 	Write-Host -Object $InfMsg -ForegroundColor 'DarkYellow'
 }
 
+function Save-PSDriveState {
+	$PSDrivesSaved = [Collections.Generic.List[Object]]::new(26)
+	$CurrentFSList = Get-PSDrive -PSProvider FileSystem | Where-Object Name -like '?'
+	foreach ($Entry in $CurrentFSList) {
+		$FSPropertyMap = @{Drive = $Entry.Name; SavedLocation = $Entry.CurrentLocation}
+		$PSDrivesSaved.Add($FSPropertyMap)
+	}
+	New-Variable -Name 'StoredPSDriveSet' -Value $PSDrivesSaved -Scope 'Script'
+}
+
+function Restore-PSDriveState {
+	$LocationStore = Get-Variable -Name 'StoredPSDriveSet' -Scope 'Script' -ValueOnly
+	foreach ($Entry in $LocationStore) {
+		$PSDriveObject = Get-PSDrive -LiteralName $Entry.Drive
+		$PSDriveObject.CurrentLocation = $Entry.SavedLocation
+	}
+	Remove-Variable -Name 'StoredPSDriveSet' -Scope 'Script'
+}
+
 function Fancy-Print {
 	param([UInt32] $Output)
 	switch ($Output) {
@@ -44,7 +63,7 @@ function Total-Print {
 			{$Output -eq 1} {$OutMsg = "$Output item has been removed in total!"; break}
 			{$Output -eq 0} {$OutMsg = 'Nothing to be found. All the things have already been cleaned, apparently!'}
 		}
-		$OutClr = ($Output -gt 0) ? [System.ConsoleColor]::Cyan : [System.ConsoleColor]::DarkGray
+		$OutClr = ($Output -gt 0) ? [ConsoleColor]::Cyan : [ConsoleColor]::DarkGray
 		$Prefix = '   =    : '
 	}
 	if ($StatusVal -eq 1) {
@@ -53,7 +72,7 @@ function Total-Print {
 			{$Output -eq 1} {$OutMsg = "$Output item has been removed so far!"; break}
 			{$Output -eq 0} {$OutMsg = 'Nothing has been deleted!'}
 		}
-		$OutClr = [System.ConsoleColor]::DarkYellow
+		$OutClr = [ConsoleColor]::DarkYellow
 		$Prefix, $OutMsg = '   !    : ', [String]::Concat($OutMsg, ' But there has been some issue in one of the locations!')
 	}
 	$OutputStr = [String]::Concat($Prefix, $OutMsg)
@@ -63,10 +82,10 @@ function Total-Print {
 function Count-Items {
 	param($Probe)
 	switch ($Probe) {
-		{$Probe -is [System.Object[]]}         {$Retv = [UInt32] $Probe.Count; break}
-		{$Probe -is [System.IO.DirectoryInfo]} {$Retv = [UInt32] 1; break}
-		{$Probe -is [System.IO.FileInfo]}      {$Retv = [UInt32] 1; break}
-		{$Probe -eq $null}                     {$Retv = [Uint32] 0}
+		{$Probe -is [Object[]]}         {$Retv = [UInt32] $Probe.Count; break}
+		{$Probe -is [IO.DirectoryInfo]} {$Retv = [UInt32] 1; break}
+		{$Probe -is [IO.FileInfo]}      {$Retv = [UInt32] 1; break}
+		{$Probe -eq $null}              {$Retv = [UInt32] 0}
 	}
 	return $Retv
 }
@@ -75,7 +94,7 @@ function Analyze-Pth {
 	param($Rsrc)
 	$Retv = [UInt32] 0
 	$Path = Resolve-Path -LiteralPath $Rsrc
-	if ($Path -is [System.Management.Automation.PathInfo]) {
+	if ($Path -is [Management.Automation.PathInfo]) {
 		$Obtained = Get-ChildItem -Path $Path -Force
 		if ($Obtained -eq $null) {
 			return $Retv
@@ -147,8 +166,8 @@ function Clean-Environ {
 # -------------------------------------------------------------- Begin Main -------------------------------------------------------------- #
 Write-Host -Object "[MNTNC] : Simple Cleaning Script -> Now Running..." -ForegroundColor $PrimColor
 $ErrActPrefSaved, $ConfrmPrefSaved = $ErrorActionPreference, $ConfirmPreference
-$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-$ConfirmPreference = [System.Management.Automation.ConfirmImpact]::None
+$ErrorActionPreference, $ConfirmPreference = 'Stop', 'None'
+Save-PSDriveState
 
 # ------------------------------------------------------ Tasks: 01 / 04 ------------------------------------------------------ #
 Write-Host -Object "[01/04] : Cleaning System Temp directory in %Windir%" -ForegroundColor $ScndColor
@@ -159,7 +178,7 @@ Write-Host -Object "[02/04] : Cleaning User's Temp directory in %LocalAppData%" 
 $Result02 = Clean-Environ -Envpth $Env:LocalAppData
 
 # ------------------------------------------------------ Tasks: 03 / 04 ------------------------------------------------------ #
-Write-Host -Object "[03/04] : Cleaning temporary processing directory (`$Env:FSWorkdir\Temp)" -ForegroundColor $ScndColor
+Write-Host -Object "[03/04] : Cleaning primary temp processing directory (`$Env:FSWorkdir\Temp)" -ForegroundColor $ScndColor
 $Result03 = Clean-Environ -Envpth $Env:FSWorkdir
 
 # ------------------------------------------------------ Tasks: 04 / 04 ------------------------------------------------------ #
@@ -183,7 +202,6 @@ if (Test-Path -LiteralPath $PathParam -PathType Container) {
 	$Items04b = Get-ChildItem -Path "tw-*.tmp" -Force
 	$CountPost04a, $CountPost04b = (Count-Items $Items04a), (Count-Items $Items04b)
 	$Result04 = ($CountInit04a + $CountInit04b) - ($CountPost04a + $CountPost04b)
-	Set-Location
 	Pop-Location -StackName 'Stack04'
 }
 else {
@@ -198,11 +216,12 @@ Write-Host -Object $StatusMsg -ForegroundColor $PrimColor
 $TotalRmvd = (Get-Variable -Name "Result*" -ValueOnly | Measure-Object -Sum).Sum
 Total-Print -Output $TotalRmvd
 
-$ErrorActionPreference = $ErrActPrefSaved
-$ConfirmPreference = $ConfrmPrefSaved
+$ErrorActionPreference, $ConfirmPreference = $ErrActPrefSaved, $ConfrmPrefSaved
 Remove-Variable -Name "Result*", "Items*", "CountInit*", "CountPost*"
 Remove-Variable -Name 'ErrActPrefSaved', 'ConfrmPrefSaved', 'StatusVal', 'StatusMsg', 'TotalRmvd', 'PathParam', 'PrimColor', 'ScndColor'
+Restore-PSDriveState
 
 $StatusMsg = "[MNTNC] : Simple Cleaning Script $($PSStyle.Italic)$($PSStyle.Foreground.Green)DONE$($PSStyle.Reset)"
 Write-Host -Object $StatusMsg -ForegroundColor 'White'
+
 # --------------------------------------------------------------- End Main --------------------------------------------------------------- #
