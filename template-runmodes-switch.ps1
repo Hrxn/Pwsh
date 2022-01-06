ec44bb2827af5690258e6c4fd24f7b353312a22d
HRXN <Hrxn@users.noreply.github.com>
2022-01-06 07:22:02 +0100

    Added a third example

--- template-runmodes-switch.ps1
+++ template-runmodes-switch.ps1
@@ -6,10 +6,10 @@ param
 
 enum RunModeType {Norun; Infos; Start}
 
-$Infocmd = 'help', 'info', '--help', '?'
+$InfoCmd = 'help', 'info', '--help', '--info', '?'
 switch ($Command) {
 	{$Command -eq 'start' -and (-not ($script:args.Count -gt 0))} {$RunMode = [RunModeType]::Start}
-	{$Command -in $Infocmd -or ($script:Help)}                    {$RunMode = [RunModeType]::Infos}
+	{$Command -in $Infocmd -or $Help}                             {$RunMode = [RunModeType]::Infos}
 	default                                                       {$RunMode = [RunModeType]::Norun}
 }
 
@@ -23,13 +23,20 @@ switch ($RunMode) {
 	Start {Write-Host '   !    Start params found, start the execution of the actual script!' -ForegroundColor 'Green'}
 }
 
-
 # Example 2 with switch statement using strongly typed enums (parentheses are needed here) (for a single-valued expression as the test: continue = break)
 
 switch ($RunMode) {
-	([RunModeType]::Norun) {Write-Host "   2    Incorrect parameter usage! | Display Info and Usage Message | $($PSStyle.Foreground.BrightCyan)command -> $command $($PSStyle.Reset)| $($PSStyle.Foreground.BrightCyan)args -> $args $($PSStyle.Reset)" -ForegroundColor 'White'; continue}
-	([RunModeType]::Infos) {Write-Host '   2    Help params found, show script help' -ForegroundColor 'Blue'; continue}
-	([RunModeType]::Start) {Write-Host '   2    Start params found, start the execution of the actual script!' -ForegroundColor 'Green'; continue}
+	([RunModeType]::Norun) {Write-Host "   -    Incorrect parameter usage! | Display Info and Usage Message | $($PSStyle.Foreground.BrightCyan)command -> $command $($PSStyle.Reset)| $($PSStyle.Foreground.BrightCyan)args -> $args $($PSStyle.Reset)" -ForegroundColor 'White'; continue}
+	([RunModeType]::Infos) {Write-Host '   ?    Help params found, show script help' -ForegroundColor 'Blue'; continue}
+	([RunModeType]::Start) {Write-Host '   !    Start params found, start the execution of the actual script!' -ForegroundColor 'Green'; continue}
+}
+
+# Example 3 with switch statement using explicte string types
+
+switch ($RunMode) {
+	'Norun' {Write-Host "   -    Incorrect parameter usage! | Display Info and Usage Message | $($PSStyle.Foreground.BrightCyan)command -> $command $($PSStyle.Reset)| $($PSStyle.Foreground.BrightCyan)args -> $args $($PSStyle.Reset)" -ForegroundColor 'White'; continue}
+	'Infos' {Write-Host '   ?    Help params found, show script help' -ForegroundColor 'Blue'; continue}
+	'Start' {Write-Host '   !    Start params found, start the execution of the actual script!' -ForegroundColor 'Green'; continue}
 }
 
 
