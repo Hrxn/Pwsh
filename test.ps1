4ae03ca3de0663d391ce5a90dbb97fd4354512f2
HRXN <Hrxn@users.noreply.github.com>
2022-01-06 06:57:25 +0100

    Added another template example

---
+++ test.ps1
@@ -0,0 +1,85 @@
+function Test-Directory ($Path) {
+	if ($null -eq $Path) {
+		return $false
+	}
+	elseif (-not (Test-Path -LiteralPath $Path)) {
+		return $false
+	}
+	elseif ((Get-Item -LiteralPath $Path) -isnot [System.IO.DirectoryInfo]) {
+		return $false
+	}
+	else {
+		return (Test-Path -LiteralPath $Path -PathType Container)
+	}
+}
+
+function Switch-SessionState {
+	param ([Parameter(Mandatory)][ValidateSet('Save', 'Change', 'Restore')][String] $Action)
+	function Save-PSDriveState {
+		$PSDrivesSaved = [Collections.Generic.List[Object]]::new(26)
+		$CurrentFSList = Get-PSDrive -PSProvider FileSystem | Where-Object Name -like '?'
+		foreach ($Entry in $CurrentFSList) {
+			$FSPropertyMap = @{Drive = $Entry.Name; SavedLocation = $Entry.CurrentLocation}
+			$PSDrivesSaved.Add($FSPropertyMap)
+		}
+		Set-Variable -Name 'StoredPSDriveSet' -Value $PSDrivesSaved -Scope 'Script'
+	}
+	function Restore-PSDriveState {
+		$LocationStore = Get-Variable -Name 'StoredPSDriveSet' -Scope 'Script' -ValueOnly
+		foreach ($Entry in $LocationStore) {
+			$PSDriveObject = Get-PSDrive -LiteralName $Entry.Drive
+			$PSDriveObject.CurrentLocation = $Entry.SavedLocation
+		}
+		Remove-Variable -Name 'StoredPSDriveSet' -Force -Scope 'Script'
+	}
+	function Save-SessionState {
+		Set-Variable -Name 'ErrActPrefSaved' -Value $ErrorActionPreference -Scope 'Script'
+		Set-Variable -Name 'ConfrmPrefSaved' -Value $ConfirmPreference -Scope 'Script'
+		Set-Variable -Name 'PresentLocation' -Value (Get-Location) -Scope 'Script'
+		Save-PSDriveState
+	}
+	function Change-SessionState {
+		Save-SessionState
+		Set-Variable -Name 'ErrorActionPreference' -Value ([Management.Automation.ActionPreference]::Stop) -Scope 'Script'
+		Set-Variable -Name 'ConfirmPreference' -Value ([Management.Automation.ConfirmImpact]::None) -Scope 'Script'
+	}
+	function Restore-SessionState {
+		Set-Variable -Name 'ErrorActionPreference' -Value $ErrActPrefSaved -Scope 'Script'
+		Set-Variable -Name 'ConfirmPreference' -Value $ConfrmPrefSaved -Scope 'Script'
+		Set-Location $PresentLocation
+		Remove-Variable -Name 'ErrActPrefSaved', 'ConfrmPrefSaved', 'PresentLocation' -Force -Scope 'Script'
+		Restore-PSDriveState
+	}
+	switch ($Action) {
+		'Save'    {Save-SessionState; break}
+		'Change'  {Change-SessionState; break}
+		'Restore' {Restore-SessionState; break}
+	}
+}
+
+function Get-Listing {
+	param ([Parameter(Mandatory)][ValidateNotNullOrEmpty()][string] $Path, [switch] $Force, [switch] $ReadOnlyCollection)
+	$ItmList, $DirItms = [System.Collections.Generic.List[System.Object]]::new(), $null
+	if (Test-Path -LiteralPath $Path -PathType Container) {
+		Push-Location -LiteralPath $Path -StackName 'list'
+		if ($PWD.Provider.Name -ceq 'FileSystem') {
+			if ($Force) {
+				$DirItms = Get-ChildItem -Force
+			}
+			else {
+				$DirItms = Get-ChildItem
+			}
+		}
+		Pop-Location -StackName 'list'
+	}
+	if ($DirItms -is [System.Object[]]) {
+		$ItmList = [System.Collections.Generic.List[System.Object]]::new($DirItms)
+	} elseif ($DirItms -is [System.IO.FileSystemInfo]) {
+		$ItmList.Add($DirItms)
+	}
+	if ($ReadOnlyCollection) {
+		$ItmList = $ItmList.AsReadOnly()
+	}
+	Write-Output $ItmList -NoEnumerate
+}
+
