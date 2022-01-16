cc81158e1f10b94af812842151143e25beef9421
HRXN <Hrxn@users.noreply.github.com>
2022-01-16 18:30:08 +0100

    Some cosmetic changes

--- vv.ffvimg.ps1
+++ vv.ffvimg.ps1
@@ -2,7 +2,7 @@ if (($args[0] -eq $null) -or ($args[1] -eq $null)) {
 	Write-Host "[vv.ffvimg] Usage: vv.ffvimg.ps1 <filename> <start position in seconds> [<duration to process in seconds>]"
 } else {
 	$File = Get-Item $args[0]
-	$Name = $file.BaseName
+	$Name = $File.BaseName
 	$tcst = $args[1]
 	if ($args[2] -ne $null) {
 		$tcen = $args[2]
@@ -13,6 +13,6 @@ if (($args[0] -eq $null) -or ($args[1] -eq $null)) {
 		Write-Host "[vv.ffvimg] Info: Deleting previously existing directory `"$Name`" ... "
 		Remove-Item -LiteralPath $Name -Recurse -Force
 	}
-	New-Item -Path "." -Name $Name -ItemType Directory
+	New-Item -Path '.' -Name $Name -ItemType 'Directory'
 	& ffmpeg -ss $tcst -t $tcen -i $File -c:v png ./$name/%08d.png
 }
