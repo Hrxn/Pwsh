1307060e4c996647f36e4e8283b075ee31a416a9
HRXN <Hrxn@users.noreply.github.com>
2022-01-16 18:28:18 +0100

    Some cosmetic changes..

--- vv.mpvimg.ps1
+++ vv.mpvimg.ps1
@@ -2,7 +2,7 @@ if (($args[0] -eq $null) -or ($args[1] -eq $null)) {
 	Write-Host "[vv.mpvimg] Usage: vv.mpvimg.ps1 <filename> <start position in seconds> [<duration to process in seconds>]"
 } else {
 	$File = Get-Item $args[0]
-	$Name = $file.BaseName
+	$Name = $File.BaseName
 	$tcst = $args[1]
 	if ($args[2] -ne $null) {
 		$tcen = $args[2]
@@ -13,6 +13,6 @@ if (($args[0] -eq $null) -or ($args[1] -eq $null)) {
 		Write-Host "[vv.mpvimg] Info: Deleting previously existing directory `"$Name`" ... "
 		Remove-Item -LiteralPath $Name -Recurse -Force
 	}
-	New-Item -Path "." -Name $Name -ItemType Directory
+	New-Item -Path '.' -Name $Name -ItemType Directory
 	& mpv --no-audio --fs --vo=image --vo-image-format=png --vo-image-png-compression=0 --vo-image-tag-colorspace=yes --vo-image-high-bit-depth=yes --vo-image-outdir=$name --start=$tcst --end=$tcen $File
 }
