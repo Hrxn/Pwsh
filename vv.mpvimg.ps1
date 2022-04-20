d6bd4610121740c5bdd8bd8a5299b9e8b946a449
HRXN <Hrxn@users.noreply.github.com>
2022-04-20 15:15:51 +0200

    Directory name refactor

--- vv.mpvimg.ps1
+++ vv.mpvimg.ps1
@@ -2,18 +2,18 @@ if ($args.Count -lt 2) {
 	Write-Host "[vv.mpvimg] Usage: vv.mpvimg.ps1 <filename> <start position in seconds> [<duration to process in seconds>]"
 } else {
 	$File = Get-Item $args[0]
-	$Name = $File.BaseName
+	$Dirn = "$($File.BaseName)-Images"
 	$tcst = $args[1]
 	if ($args.Count -eq 3) {
 		$tcen = $args[2]
 	} else {
 		$tcen = $args[1] + 2
 	}
-	if (Test-Path -LiteralPath $Name -PathType Container) {
-		Write-Host "[vv.mpvimg] Info: Deleting previously existing directory `"$Name`" ... "
-		Remove-Item -LiteralPath $Name -Recurse -Force
+	if (Test-Path -LiteralPath $Dirn -PathType Container) {
+		Write-Host "[vv.mpvimg] Info: Deleting previously existing directory ""$Dirn"" ... "
+		Remove-Item -LiteralPath $Dirn -Recurse -Force
 	}
-	New-Item -Path '.' -Name "${Name}-Images" -ItemType 'Directory'
+	New-Item -Path '.' -Name $Dirn -ItemType 'Directory'
 	$vo_image_opts = '--vo-image-format=png','--vo-image-png-compression=0','--vo-image-tag-colorspace=yes','--vo-image-high-bit-depth=yes'
-	& mpv --quiet --no-audio --fs --vo=image $vo_image_opts --vo-image-outdir=$Name --start=$tcst --end=$tcen $File
+	& mpv --quiet --no-audio --fs --vo=image $vo_image_opts --vo-image-outdir=$Dirn --start=$tcst --end=$tcen $File
 }
