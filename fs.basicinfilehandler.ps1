07ef9568c6a9b3f6a5ea2343ec438432e1c73542
HRXN <Hrxn@users.noreply.github.com>
2022-04-28 23:16:57 +0200

    Prettify messages..

--- fs.basicinfilehandler.ps1
+++ fs.basicinfilehandler.ps1
@@ -1,20 +1,20 @@
 if ($args.Count -lt 2) {
-	Write-Host "[fs.basicinfilehandler] Usage: fs.infilehandler.ps1 <INPUT-FILE> <PROCESSING-SCRIPT>"
+	Write-Host "[fs.basicinfilehandler] Usage: fs.basicinfilehandler.ps1 <INPUT-FILE> <PROCESSING-SCRIPT>"
 	exit 0
 }
 
 if (-not (Test-Path -LiteralPath $args[0] -PathType Leaf)) {
-	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file"
+	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file" -ForegroundColor DarkRed
 	exit 1
 }
 
 if (-not ((Get-Content $args[0] | Measure-Object -Line).Lines -gt 0)) {
-	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty"
+	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty" -ForegroundColor DarkRed
 	exit 2
 }
 
 if (-not (Get-Command -Name $args[1] -ErrorAction Ignore)) {
-	Write-Host "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist"
+	Write-Host "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist" -ForegroundColor DarkRed
 	exit 3
 }
 
