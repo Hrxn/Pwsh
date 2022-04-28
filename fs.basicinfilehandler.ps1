8480d7d1c9a12564e2b2c52e92ba304e45581598
HRXN <Hrxn@users.noreply.github.com>
2022-04-28 23:00:37 +0200

    Strict-Mode compat

--- fs.basicinfilehandler.ps1
+++ fs.basicinfilehandler.ps1
@@ -1,4 +1,4 @@
-if (($null -eq $args[0]) -or ($null -eq $args[1])) {
+if ($args.Count -lt 2) {
 	Write-Host "[fs.basicinfilehandler] Usage: fs.infilehandler.ps1 <INPUT-FILE> <PROCESSING-SCRIPT>"
 	exit 0
 }
@@ -8,12 +8,12 @@ if (-not (Test-Path -LiteralPath $args[0] -PathType Leaf)) {
 	exit 1
 }
 
-if (-not ((Get-Content $args[0]).Count -gt 0)) {
+if (-not ((Get-Content $args[0] | Measure-Object -Line).Lines -gt 0)) {
 	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty"
 	exit 2
 }
 
-if (-not (Get-Command $args[1])) {
+if (-not (Get-Command -Name $args[1] -ErrorAction Ignore)) {
 	Write-Host "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist"
 	exit 3
 }
