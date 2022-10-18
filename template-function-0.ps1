b8352a04aad269c2a92521cab4d05cedcec5430b
HRXN <Hrxn@users.noreply.github.com>
2022-10-18 14:12:44 +0200

    Improve this example too

--- template-function-0.ps1
+++ template-function-0.ps1
@@ -43,39 +43,67 @@ param(
 
 begin {
 
-	function Send-Exception ([string] $ExceptionID, [string[]] $InfoCol) {
-		$NamePrfx = '[Scriptname]'
-		$ColrCatn = $PSStyle.Foreground.White
-		$ColrBase = $PSStyle.Foreground.BrightBlack
-		$ColrEmph = $PSStyle.Foreground.BrightWhite
-		switch ($ExceptionID) {
-			'exc-info-01' {
-				$ExcMsgPrfx = "${NamePrfx} ${ColrCatn}Info${ColrBase} :"
-				$ExcMessage = "${ExcMsgPrfx} Somehow ""${ColrEmph}$($InfoCol[0])${ColrBase}"" is not as expected."
-				$ExceptType = [System.NotImplementedException]::new($ExcMessage)
-				$ExitNumber, $ExcBasecat = 1, 'DarkMagenta'
+	function Show-Status ([String] $ID, [String[]] $Text) {
+		switch ($ID) {
+			{$_.StartsWith('exc')} {$vtC1 = $ccExce}
+			{$_.StartsWith('err')} {$vtC1 = $ccErro}
+			{$_.StartsWith('wrn')} {$vtC1 = $ccWarn}
+			{$_.StartsWith('inf')} {$vtC1 = $ccInfo}
+			{$_.StartsWith('log')} {$vtC1 = $ccLogm}
+			{$_.StartsWith('sts')} {$vtC1 = $ccHigh}
+		}
+		switch ($ID) {
+			'err-ca-01' { $Msg = "${vtC1}Error ${ccZero}: The specified argument for parameter ${ccSubt}-Path ${ccZero}" +
+				"(${ccShft}'${ccHigh}$($Text[1])${ccShft}'${ccZero}) does not exist as a valid literal path!"
 			}
-			'exc-warn-01' {
-				$ExcMsgPrfx = "${NamePrfx} ${ColrCatn}Warning${ColrBase} :"
-				$ExcMessage = "${ExcMsgPrfx} It's ""${ColrEmph}$($InfoCol[1])${ColrBase}"" unavailable, " +
-								"while ""${ColrEmph}$($InfoCol[0])${ColrBase}"" happened."
-				$ExceptType = [System.NotSupportedException]::new($ExcMessage)
-				$ExitNumber, $ExcBasecat = 2, 'DarkYellow'
+			'err-ca-02' { $Msg = "${vtC1}Error ${ccZero}: The specified argument for parameter ${ccSubt}-Path ${ccZero}" +
+				"(${ccShft}'${ccHigh}$($Text[1])${ccShft}'${ccZero}) does not refer to an existing directory!"
 			}
-			'exc-erro-01' {
-				$ExcMsgPrfx = "${NamePrfx} ${ColrCatn}Exception${ColrBase} :"
-				$ExcMessage = "${ExcMsgPrfx} An error in ""${ColrEmph}$($InfoCol[0])${ColrBase}"" occurred."
-				$ExceptType = [System.NotImplementedException]::new($ExcMessage)
-				$ExitNumber, $ExcBasecat = 3, 'DarkRed'
+			'wrn-ca-01' { $Msg = "${vtC1}Error ${ccZero}: The specified argument for parameter ${ccSubt}-Path ${ccZero}" +
+				"(${ccShft}'${ccHigh}$($Text[1])${ccShft}'${ccZero}) could not be resolved!"
 			}
-			'exc-crit-01' {
-				$ExcMsgPrfx = "${NamePrfx} ${ColrCatn}Error${ColrBase} :"
-				$ExcMessage = "${ExcMsgPrfx} An critical error occured!"
-				$ExceptType = [System.NotSupportedException]::new($ExcMessage)
-				$ExitNumber, $ExcBasecat = 4, 'Red'
+			'wrn-ca-02' { $Msg = "${vtC1}Error ${ccZero}: The specified argument for parameter ${ccSubt}-$($Text[1])${ccZero} " +
+				"(${ccShft}'${ccHigh}$($Text[2])${ccShft}'${ccZero}) could not be found as a file!"
+			}
+			'inf-ca-01'  { $Msg = "${vtC1}Info ${ccZero}: The file ${ccShft}'${ccHigh}$($Text[1])${ccShft}'${ccZero} has been " +
+				"successfully replaced with the file ${ccShft}'${ccHigh}$($Text[2])${ccShft}'${ccZero} while retaining its filename!"
 			}
 		}
-		Write-Host -Object $ExcMessage -ForegroundColor $ExcBasecat
+		$Prefix = "${ccCats}[${vtC1}$($Text[0])${ccCats}]${ccZero}"
+		$Output = [String]::Concat($Prefix, ' > ', $Msg)
+		Write-Host -Object $Output
+	}
+
+	function Send-Exception ([String] $ExceptionID, [String[]] $InfoCol) {
+		$NamePrfx = '[Scriptname]'
+		switch ($ExceptionID) {
+			'exc-info-01' {
+				$ExcMsgPrfx = "${NamePrfx} ${ccInfo}Info${ccZero} :"
+				$ExcMessage = "${ExcMsgPrfx} Somehow ""${ccEmph}$($InfoCol[0])${ccZero}"" is not as expected."
+				$ExceptType = [System.NotImplementedException]::new($ExcMessage)
+				$ExitNumber = 1
+			}
+			'exc-warn-01' {
+				$ExcMsgPrfx = "${NamePrfx} ${ccInfo}Warning${ccZero} :"
+				$ExcMessage = "${ExcMsgPrfx} It's ""${ccEmph}$($InfoCol[1])${ccZero}"" unavailable, " +
+								"while ""${ccEmph}$($InfoCol[0])${ccZero}"" happened."
+				$ExceptType = [System.NotSupportedException]::new($ExcMessage)
+				$ExitNumber = 2
+			}
+			'exc-erro-01' {
+				$ExcMsgPrfx = "${NamePrfx} ${ccInfo}Exception${ccZero} :"
+				$ExcMessage = "${ExcMsgPrfx} An error in ""${ccEmph}$($InfoCol[0])${ccZero}"" occurred."
+				$ExceptType = [System.NotImplementedException]::new($ExcMessage)
+				$ExitNumber = 3
+			}
+			'exc-crit-01' {
+				$ExcMsgPrfx = "${NamePrfx} ${ccInfo}Error${ccZero} :"
+				$ExcMessage = "${ExcMsgPrfx} An critical error occured!"
+				$ExceptType = [System.NotSupportedException]::new($ExcMessage)
+				$ExitNumber = 4
+			}
+		}
+		Show-Status -ID $ExcMessage -Text $InfoCol
 		if ($Raise) {
 			throw $ExceptType
 		}
@@ -84,22 +112,9 @@ begin {
 		}
 	}
 
-	function Show-Message ([string] $MessageID, [string[]] $InfoCol) {
-		$NamePrfx = '[Scriptname]'
-		$ColrBase = $PSStyle.Foreground.BrightBlack
-		$ColrEmph = $PSStyle.Foreground.BrightWhite
-		switch ($MessageID) {
-			'msg-info-01' {
-				$MessageStr = "${NamePrfx} I'm a message text."
-			}
-			'msg-hint-01' {
-				$MessageStr = "${NamePrfx} I'm an information hint."
-			}
-		}
-		Write-Host -Object $MessageStr
-	}
 
-	function Write-Log ([string] $LogID, [string[]] $InfoCol) {
+
+	function Write-Log ([String] $LogID, [String[]] $InfoCol) {
 		if ($null -ne $Env:FSPS_Logdir) {
 			$Logdir = $Env:FSPS_Logdir
 		} elseif ($PWD.Provider.Name -ceq 'FileSystem') {
@@ -107,7 +122,7 @@ begin {
 		} else {
 			$Logdir = [Path]::GetTempPath()
 		}
-		$Logdir = ([Path]::EndsInDirectorySeparator($Logdir)) ? $Logdir : [System.String]::Concat($Logdir, [Path]::DirectorySeparatorChar)
+		$Logdir = ([Path]::EndsInDirectorySeparator($Logdir)) ? $Logdir : [String]::Concat($Logdir, [Path]::DirectorySeparatorChar)
 		$Logsrc = (Split-Path $PSCommandPath -Leaf)
 		$Log_s1 = ('-' * 100) + "`n"
 		$Log_s2 = ('-' * 140) + "`n"
@@ -116,13 +131,13 @@ begin {
 				$LogCatName = 'standard'
 				$LogHeading = ">> $((Get-Date).ToString()) << | $Logsrc -> logfile: $LogCatName"
 				$LogMessage = "Write something into a log file... like ""$($InfoCol[0])"" even if nothing special has happened?"
-				$LogContent = [System.String]::Concat($Log_s1, $LogHeading, "`n", $Log_s2, $LogMessage, "`n", $Log_s2)
+				$LogContent = [String]::Concat($Log_s1, $LogHeading, "`n", $Log_s2, $LogMessage, "`n", $Log_s2)
 			}
 			'log-issues' {
 				$LogCatName = 'issues'
 				$LogHeading = ">> $((Get-Date).ToString()) << | $Logsrc -> logfile: $LogCatName"
 				$LogMessage = "There has been a problem with the collection ""$($InfoCol)"" !"
-				$LogContent = [System.String]::Concat($Log_s1, $LogHeading, "`n", $Log_s2, $LogMessage, "`n", $Log_s2)
+				$LogContent = [String]::Concat($Log_s1, $LogHeading, "`n", $Log_s2, $LogMessage, "`n", $Log_s2)
 			}
 		}
 		Add-Content -Value $LogContent -LiteralPath "${Logdir}logfile__${Logsrc}__${LogCatName}.txt"
