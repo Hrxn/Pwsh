986a4c8ff626f3051da711645a989c0b29279046
HRXN <Hrxn@users.noreply.github.com>
2022-12-02 16:23:39 +0100

    Avoid using Write-Host..

--- fs.basicinfilehandler.ps1
+++ fs.basicinfilehandler.ps1
@@ -1,20 +1,20 @@
 if ($args.Count -lt 2) {
-	Write-Host "[fs.basicinfilehandler] Usage: fs.basicinfilehandler.ps1 <INPUT-FILE> <PROCESSING-SCRIPT>"
+	Write-Output "[fs.basicinfilehandler] Usage: fs.basicinfilehandler.ps1 <INPUT-FILE> <PROCESSING-SCRIPT>"
 	exit 0
 }
 
 if (-not (Test-Path -LiteralPath $args[0] -PathType Leaf)) {
-	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file" -ForegroundColor DarkRed
+	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file" -ForegroundColor DarkRed
 	exit 1
 }
 
 if (-not ((Get-Content $args[0] | Measure-Object -Line).Lines -gt 0)) {
-	Write-Host "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty" -ForegroundColor DarkRed
+	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty" -ForegroundColor DarkRed
 	exit 2
 }
 
 if (-not (Get-Command -Name $args[1] -ErrorAction Ignore)) {
-	Write-Host "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist" -ForegroundColor DarkRed
+	Write-Output "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist" -ForegroundColor DarkRed
 	exit 3
 }
 
@@ -24,7 +24,7 @@ $File = (Get-Item $args[0]).FullName
 $Cntr = [UInt64] 0
 
 foreach ($Read in [System.IO.File]::ReadLines($File)) {
-	$Cntr += 1
+	$Cntr++
 	$Line = $Read.Trim()
 	if (-not ($Line.StartsWith('#') -or $Line.StartsWith('-') -or [System.String]::IsNullOrEmpty($Line))) {
 		& $Proc $Line
