ea94ad29c50cf303fa75ac1d872cfa5d679dc974
HRXN <Hrxn@users.noreply.github.com>
2022-12-02 16:46:40 +0100

    Consistency to the other stuff..

--- fs.filetypepresence.ps1
+++ fs.filetypepresence.ps1
@@ -1,28 +1,30 @@
-[CmdletBinding(PositionalBinding = $false)]
+[CmdletBinding(PositionalBinding=$false)]
 param
 (
 	[Parameter()][switch] $Recurse,
-	[Parameter(Position = 0)][string] $Path = $PWD
+	[Parameter(Position=0)][string] $Path = $PWD
 )
 
-if (($Path -eq '--help' -or $Path -eq '?') -or [String]::IsNullOrEmpty($Path)) {
-	Write-Host "[fs.filetypepresence] Usage: fs.filetypepresence.ps1 [-Path] <PATH> [-Recurse]"
+if (($Path -eq '--help' -or $Path -eq '?') -or [string]::IsNullOrWhiteSpace($Path)) {
+	Write-Output "[fs.filetypepresence] Usage: fs.filetypepresence.ps1 [-Path] <PATH> [-Recurse]"
 	exit 0
 }
 if (-not (Test-Path -LiteralPath $Path -PathType Container)) {
-	Write-Host "[fs.filetypepresence] The given path parameter ""$Path"" does not exist or is not a valid path!" -ForegroundColor DarkRed
+	Write-Output "[fs.filetypepresence] The given path parameter ""$Path"" does not exist or is not a valid path!"
 	exit 1
 }
 
 $Path = Convert-Path -LiteralPath $Path
+
 if ($Recurse) {
-	$Full = Get-ChildItem -Path $Path -Recurse -Force -File
+	$Full = Get-ChildItem $Path -Recurse -Force -File
 } 
 else {
-	$Full = Get-ChildItem -Path $Path -Force -File
+	$Full = Get-ChildItem $Path -Force -File
 }
+
 $Size = ($Full | Measure-Object).Count
-$Exts = [Collections.Generic.List[String]]::new()
+$Exts = [System.Collections.Generic.List[System.String]]::new()
 
 foreach ($Item in $Full) {
 	if (-not ($Exts.Contains($Item.Extension))) {
@@ -30,17 +32,17 @@ foreach ($Item in $Full) {
 	}
 }
 
-if ($Exts.Contains([String]::Empty)) {
-	$Exts[$Exts.IndexOf([String]::Empty)] = '(No Extension)'
+if ($Exts.Contains([string]::Empty)) {
+	$Exts[$Exts.IndexOf([string]::Empty)] = '(No Extension)'
 }
 
-[Console]::WriteLine('────────────────────────────────────────────────────────────────')
-[Console]::WriteLine('── The following File Types / File Extensions have been found ──')
-[Console]::WriteLine('────────────────────────────────────────────────────────────────')
+[System.Console]::WriteLine('────────────────────────────────────────────────────────────────')
+[System.Console]::WriteLine('── The following File Types / File Extensions have been found ──')
+[System.Console]::WriteLine('────────────────────────────────────────────────────────────────')
 foreach ($Type in $Exts) {
-	[Console]::WriteLine('    ' + $Type)
+	[System.Console]::WriteLine('    ' + $Type)
 }
-[Console]::WriteLine('────────────────────────────────────────────────────────────────')
-[Console]::WriteLine("Total Files: $Size")
-[Console]::WriteLine('───────────────────────────────')
+[System.Console]::WriteLine('────────────────────────────────────────────────────────────────')
+[System.Console]::WriteLine("Total Files: $Size")
+[System.Console]::WriteLine('───────────────────────────────')
 
