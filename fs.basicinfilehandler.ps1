d5a5a94d0a1337ad1e79c674e2089f20410239e2
HRXN <Hrxn@users.noreply.github.com>
2022-12-04 15:20:18 +0100

    Unsupported parameter in this case..

--- fs.basicinfilehandler.ps1
+++ fs.basicinfilehandler.ps1
@@ -4,17 +4,17 @@ if ($args.Count -lt 2) {
 }
 
 if (-not (Test-Path -LiteralPath $args[0] -PathType Leaf)) {
-	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file" -ForegroundColor DarkRed
+	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file"
 	exit 1
 }
 
 if (-not ((Get-Content $args[0] | Measure-Object -Line).Lines -gt 0)) {
-	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty" -ForegroundColor DarkRed
+	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty"
 	exit 2
 }
 
 if (-not (Get-Command -Name $args[1] -ErrorAction Ignore)) {
-	Write-Output "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist" -ForegroundColor DarkRed
+	Write-Output "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist"
 	exit 3
 }
 
