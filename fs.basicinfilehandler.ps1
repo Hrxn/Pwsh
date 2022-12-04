aa771219e33367739fa944f2d4c51271dc0883bf
HRXN <Hrxn@users.noreply.github.com>
2022-12-04 16:14:41 +0100

    Use some ANSI sequences..

--- fs.basicinfilehandler.ps1
+++ fs.basicinfilehandler.ps1
@@ -1,20 +1,20 @@
 if ($args.Count -lt 2) {
-	Write-Output "[fs.basicinfilehandler] Usage: fs.basicinfilehandler.ps1 <INPUT-FILE> <PROCESSING-SCRIPT>"
+	Write-Output "`e[90m[`e[97mfs.basicinfilehandler`e[90m]`e[0m `e[32mUsage`e[90m:`e[0m fs.basicinfilehandler.ps1 `e[90m<`e[94mINPUT-FILE`e[90m> `e[90m<`e[94mPROCESSING-SCRIPT`e[90m>`e[0m"
 	exit 0
 }
 
 if (-not (Test-Path -LiteralPath $args[0] -PathType Leaf)) {
-	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> parameter could not be found as a file"
+	Write-Output "`e[90m[`e[97mfs.basicinfilehandler`e[90m]`e[0m `e[31mError`e[90m:`e[0m Given `e[90m<`e[94mINPUT-FILE`e[90m>`e[0m parameter could not be found as a file!"
 	exit 1
 }
 
 if (-not ((Get-Content $args[0] | Measure-Object -Line).Lines -gt 0)) {
-	Write-Output "[fs.basicinfilehandler] Given <INPUT-FILE> has been found but seems to be empty"
+	Write-Output "`e[90m[`e[97mfs.basicinfilehandler`e[90m]`e[0m `e[31mError`e[90m:`e[0m Given `e[90m<`e[94mINPUT-FILE`e[90m>`e[0m has been found but seems to be empty!"
 	exit 2
 }
 
 if (-not (Get-Command -Name $args[1] -ErrorAction Ignore)) {
-	Write-Output "[fs.basicinfilehandler] Given <PROCESSING-SCRIPT> command does not exist"
+	Write-Output "`e[90m[`e[97mfs.basicinfilehandler`e[90m]`e[0m `e[31mError`e[90m:`e[0m Given `e[90m<`e[94mPROCESSING-SCRIPT`e[90m>`e[0m command does not exist!"
 	exit 3
 }
 
